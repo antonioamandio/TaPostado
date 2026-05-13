fd3119524f9f19265f9e37a9f5e6c120db90d133
António Amândio <antonioamandio22gmail.com>
2026-05-13 20:26:39 +0100

    feat: initialize blog API with Express and add health check endpoint

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const PORT = 3333
+
+const app = express()
+
+app.get('/health', (req, res) => {
+    res.json({ message: 'Servidor rodando!', status: true })
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
