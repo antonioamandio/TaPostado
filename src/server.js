09d1feb9cd23323f4d77bde220527fffd207f5bc
António Amândio <antonioamandio22gmail.com>
2026-05-13 21:04:04 +0100

    feat: add .env.example file and update server configuration for environment variables

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 import express from 'express'
 
-const PORT = 3333
-
+const PORT = process.env.PORT || 8888
 const app = express()
 
 app.get('/health', (req, res) => {
     res.json({ message: 'Servidor rodando!', status: true })
 })
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
