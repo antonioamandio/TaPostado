85b6c1048d6ccbe3ffe2943a91d132471929b52c
antonioamandio <antonioamandio22@gmail.com>
2026-06-15 17:12:34 +0100

    feat: create database configuration file for Prisma client with PostgreSQL adapter
    - Modify Prisma generator provider and update datasource configuration
    
    - Update package.json to include Prisma dependencies and add API keyword
    
    - Add integrity check endpoints and temporary user endpoints for database connection testing to the server.js file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import express from 'express'
+import { prisma } from './config/database.js'
 
 const PORT = process.env.PORT || 3333
 const app = express()
 
+// Endpoint to test API connection
 app.get('/health', (req, res) => {
-    res.json({
-        message: `Servidor rodando em: ${process.env.URL || 'http://localhost'}:${PORT}`,
-        status: true,
-    })
+    try {
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
-app.listen(PORT)
+// Temporary endpoint to test DB connection
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+
+    return res.json(users)
+})
+
+app.listen(PORT, () => {
+    console.log(
+        `Server running on: ${process.env.URL || 'http://localhost'}:${PORT}`,
+    )
+})
