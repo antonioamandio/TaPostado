dc08d908e1fbffe36e996a4eb9e8dea1be367c47
antonioamandio <antonioamandio22@gmail.com>
2026-07-14 19:15:18 +0100

    fix(server.js): correct usage of express.json() middleware and remove commented-out user endpoints

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { userRepository } from './container.js'
 const PORT = process.env.PORT || 3333
 const app = express()
 
-app.user(express.json())
+app.use(express.json())
 
 // Endpoint to test API connection
 app.get('/health', (req, res) => {
@@ -13,26 +13,6 @@ app.get('/health', (req, res) => {
     } catch (error) {
         res.status(500).send()
     }
-})
-
-/*
-// Temporary endpoint for testing - GET /users
-app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany()
-
-    return res.json(users)
-})
-*/
-
-// Temporary endpoint for testing - POST /users
-app.post('/users', async (req, res) => {
-    const user = await userRepository.create({
-        name: 'Antonio Amandio',
-        email: 'email de testeeeeeeeee',
-        password: 'Os repositories estao a funcionar',
-    })
-
-    return res.status(201).json(user)
 })
 
 app.listen(PORT, () => {
