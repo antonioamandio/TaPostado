0e44be1ed377818568af437bf60ae9c5c56d0f29
antonioamandio <antonioamandio22@gmail.com>
2026-07-18 22:34:35 +0100

    feat(CommentRepository): add delete method to remove a comment by its ID

--- src/repositories/CommentRepository.js
+++ src/repositories/CommentRepository.js
@@ -25,4 +25,10 @@ export class CommentRepository {
             data,
         })
     }
+
+    async delete(id) {
+        return this.#prisma.comment.delete({
+            where: { id },
+        })
+    }
 }
