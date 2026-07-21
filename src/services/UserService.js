0dd617bc9d7aeb9097961433bc4f57018e3b598e
antonioamandio <antonioamandio22@gmail.com>
2026-07-21 18:40:54 +0100

    feat(UserService): implement UserService class with CRUD methods

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,1 +1,31 @@
-export class UserService {}
+export class UserService {
+    #userRepository
+
+    constructor(userRepository) {
+        this.#userRepository = userRepository
+    }
+
+    async create(data) {
+        return this.#userRepository.create(data)
+    }
+
+    async findAll() {
+        return this.#userRepository.findAll()
+    }
+
+    async findByEmail(email) {
+        return this.#userRepository.findByEmail(email)
+    }
+
+    async findById(id) {
+        return this.#userRepository.findById(id)
+    }
+
+    async update(id, data) {
+        return this.#userRepository.update(id, data)
+    }
+
+    async delete(id) {
+        return this.#userRepository.delete(id)
+    }
+}
